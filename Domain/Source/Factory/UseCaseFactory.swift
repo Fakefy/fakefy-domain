05515f548e933e4424991da4342edd9988632f97
neymoura <neymourac@gmail.com>
2021-11-17 09:38:47 -0300

    UseCaseFactory is now public

--- Domain/Source/Factory/UseCaseFactory.swift
+++ Domain/Source/Factory/UseCaseFactory.swift
@@ -5,7 +5,7 @@
 //  Created by Ney Moura on 10/06/21.
 //
 
-class UseCaseFactory {
+public class UseCaseFactory {
     public static func makeSearchUseCase(searchRepository: SearchRepository) -> SearchAlbumUseCase {
         return SearchAlbumUseCaseImpl(searchRepository: searchRepository)
     }
